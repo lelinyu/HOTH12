4953f45a16f007e1264dc6cf4eb013f62540f2bd
Kevin <kevinjcao11888@gmail.com>
2025-03-02 12:03:46 -0800

    new

--- volunteer-app/src/App.jsx
+++ volunteer-app/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1> Welcome to the Webpage </h1>
+      <h1 className = "top-left"> Welcome to the Webpage </h1>
     </div>
   );
 }
