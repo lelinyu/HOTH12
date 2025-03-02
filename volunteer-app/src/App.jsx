e0fa56ce8f07fdee4c89ef41f16305a430722700
Joshua Jin <4928anzhe@gmail.com>
2025-03-02 12:19:26 -0800

    add navbar

--- volunteer-app/src/App.jsx
+++ volunteer-app/src/App.jsx
@@ -2,9 +2,17 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1> Welcome to the Webpage </h1>
-    </div>
+    <>
+    <nav class="navbar">
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Opportunities</a></li>
+        <li><a href="">Saved</a></li>
+      </ul>
+    </nav><div>
+        <h1> Welcome to the Webpage </h1>
+      </div>
+    </>
   );
 }
 
