337dd374a0e15e8e7f10fb99a10528fd4b93d32e
Joshua Jin <4928anzhe@gmail.com>
2025-03-02 14:05:57 -0800

    navbar class, home class, css stuff

--- volunteer-app/src/App.jsx
+++ volunteer-app/src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import Navbar from "./components/navbar";
+import Home from "./pages/Home";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
 
 function App() {
   return (
     <>
     <div>
+      <Router>
       <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home}/>
+      </Switch>
+      </Router>
       <h1 className = "top-left"> Welcome to the Webpage </h1>
     </div>
     </>
--- volunteer-app/src/components/navbar.jsx
+++ volunteer-app/src/components/navbar.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import BearLogo from "../assets/bruinbear.jpeg";
+import {Link} from "react-router-dom";
+import "../styles/navbar.css";
 
 function navbar() {
     return (
@@ -8,11 +10,9 @@ function navbar() {
             <img src={BearLogo}></img>
         </div>
         <div className = "rightSide">
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Opportunities</a></li>
-                <li><a href="">Saved</a></li>
-            </ul>
+            <Link to="/"> Home </Link>
+            <Link to="/opportunities"> Opportunities </Link>
+            <Link to="/saved"> Saved </Link>
         </div>
     </nav>
     )
