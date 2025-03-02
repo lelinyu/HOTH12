654b9123e1ea0334ae123983363679625c8ba64d
Joshua Jin <4928anzhe@gmail.com>
2025-03-02 11:40:56 -0800

    Testing top bar for frontend

--- volunteer-app/src/App.jsx
+++ volunteer-app/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div id = "top-bar">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
