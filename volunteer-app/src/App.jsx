2e83a67fe6b0f19acec983d2b36f1f75d74316ec
Bryan Yu <54421861+lelinyu@users.noreply.github.com>
2025-03-02 16:15:14 -0800

    Opportunities page

--- volunteer-app/src/App.jsx
+++ volunteer-app/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Opportunity from "./pages/Opportunity"; // Import the Opportunity component
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" exact element={Home}/>
+        <Route path="/opportunities" element={<Opportunity />} />
       </Routes>
       </Router>
     </div>
---
+++ volunteer-app/src/pages/Opportunity.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Opportunity() {
+    return (
+        <div>
+            <h1>Opportunities Page</h1>
+            <p>Here you will find various opportunities.</p>
+        </div>
+    );
+}
+
+export default Opportunity;
